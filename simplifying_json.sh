0be5919059ac86f3883e63246a1efc4d20693bab
jsantanna <jair.santanna@northwave.nl>
2020-06-26 09:05:27 +0200

    organizing repo

--- simplifying_json.sh
+++ simplifying_json.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cat shodan_list_daily_files.txt | while read filename b c link
+echo "Please enter your Shodan API key, followed by [ENTER]:"
+read shodankey
+
+shodan init $shodankey
+
+shodan data list --dataset=raw-daily > shodan_list_daily_files.txt
+
+cat shodan_list_daily_files.txt | while read filename filesize id link
 do 
 	echo $filename
 	aria2c -x4 -s4 -o shodan_raw_$filename $link
